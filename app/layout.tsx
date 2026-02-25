5bc2465bbe0fc3380de98ae0e30ea15f6a7e7cb1
Murillo Augusto <tech@basisaceleradora.com.br>
2026-02-25 11:16:47 -0300

    feat: pixel facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={`${_inter.variable} ${_playfair.variable}`}>
-      <body className="font-sans antialiased">{children}</body>
+      <head>
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '769213809532466');
+        fbq('track', 'PageView');` }} />
+      </head>
+      <body className="font-sans antialiased">
+        {/* Meta Pixel NoScript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=769213809532466&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
